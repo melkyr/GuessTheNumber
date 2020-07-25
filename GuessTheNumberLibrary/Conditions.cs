da2af12efbd4cc37e920b6ca64c65c145871308a
Andres Hernandez <mirragemelkyr@gmail.com>
2020-07-25 16:15:51 -0500

    Initial upload

---
+++ GuessTheNumberLibrary/Conditions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuessTheNumberLibrary
+{
+    public class Conditions
+    {
+
+        public int UpNumber
+        {
+            get { return _upNumber; }
+            set { _upNumber = value; }
+        }
+
+        public int LowNumber
+        {
+            get { return _lowNumber; }
+            set { _lowNumber = value; }
+        }
+
+        public int NumberToGuess { get => _numberToGuess; }
+
+        private int _numberToGuess;
+
+        public void generateNumber(int low, int high)
+        {
+            Random myRandom = new Random();
+            _numberToGuess=myRandom.Next(low,high);
+        }
+
+        private int _lowNumber;
+        private int _upNumber;
+
+
+    }
+}
---
+++ GuessTheNumberLibrary/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuessTheNumberLibrary
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public Dictionary<string, int> Score { get; set; }
+
+        public Player()
+        {
+            Score.Add("Wins", 0);
+            Score.Add("Loses", 0);
+        }
+    }
+}
